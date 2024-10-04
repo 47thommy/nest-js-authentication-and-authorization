e6cb5cbb1ab46dea968ecb78d3419211346af7eb
47thommy <47thommy@gmail.com>
2024-10-04 16:19:54 +0300

    add the user entity and register it

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [User],
   controllers: [UsersController],
   providers: [UsersService],
 })
